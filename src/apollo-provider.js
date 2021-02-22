00ca281a7bf1bb43fa0fc46facc3b9e536108ff4
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-02-22 10:45:53 -0700

    edited server url

--- src/apollo-provider.js
+++ src/apollo-provider.js
@@ -6,7 +6,7 @@ import { ApolloProvider } from "@apollo/react-hooks"
 import { setContext } from "apollo-link-context"
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:5000"
+    uri: "https://hikespot-backend.herokuapp.com/"
 })
 
 const authLink = setContext(() => {
